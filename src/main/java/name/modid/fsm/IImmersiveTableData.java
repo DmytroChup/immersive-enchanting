db0d4c030f095e4ce4ddf50d2e3b0bbdc8382ab0
Dmytro Chupryna <dmytro.chupryna18@gmail.com>
2026-05-01 20:08:08 +0200

    Implement FSM for enchanting table

---
+++ src/main/java/name/modid/fsm/IImmersiveTableData.java
@@ -0,0 +1,13 @@
+package name.modid.fsm;
+
+import net.minecraft.world.item.ItemStack;
+
+public interface IImmersiveTableData {
+    TableState getState();
+    void transitionTo(TableState newState);
+
+    ItemStack getTargetItem();
+    void setTargetItem(ItemStack stack);
+
+    void syncToClients();
+}
---
+++ src/main/java/name/modid/fsm/TableState.java
@@ -0,0 +1,9 @@
+package name.modid.fsm;
+
+public enum TableState {
+    IDLE,             // The table is empty, waiting for an item
+    ITEM_INSERTED,    // An item on the altar; the runes haven't appeared yet
+    RUNE_SELECTION,   // Runes are loaded (rendered), waiting for clicks (raycast)
+    CRAFTING,         // The enchantment animation plays
+    FAILED            // Error (invalid combination or insufficient resources)
+}
